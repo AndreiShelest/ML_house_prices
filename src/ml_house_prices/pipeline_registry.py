e5e8fd2dc3df1a196f8ef5b9c89c00184bd6d9d2
Zofia Bracha <zbracha49@gmail.com>
2025-01-12 17:45:23 +0100

    progress with preprocessing and modelling

--- src/ml_house_prices/pipeline_registry.py
+++ src/ml_house_prices/pipeline_registry.py
@@ -4,6 +4,7 @@ from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
 from ml_house_prices.pipelines.splitting import pipeline as splitting_pipeline
 from ml_house_prices.pipelines.data_processing import pipeline as data_processing_pipeline
+from ml_house_prices.pipelines.modelling import pipeline as modelling_pipeline
 
 def register_pipelines() -> dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -13,10 +14,12 @@ def register_pipelines() -> dict[str, Pipeline]:
     """
     splitting = splitting_pipeline.create_splitting_pipeline()
     data_processing = data_processing_pipeline.create_data_processing_pipeline()
+    modelling = modelling_pipeline.create_pipeline_model
 
     # Register pipelines
     return {
-        "__default__": splitting + data_processing,  # Combine pipelines into the default pipeline
-        "data_splitting_pipeline": splitting,        # Register splitting pipeline
-        "data_processing_pipeline": data_processing # Register data processing pipeline
+        "__default__": splitting + data_processing + modelling,
+        "data_splitting_pipeline": splitting,       
+        "data_processing_pipeline": data_processing,
+        "modelling_pipeline": modelling
     }
